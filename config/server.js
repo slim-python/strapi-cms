360abee5cb96e0cffe22c0c4143d2ad79dd0c172
slim.python <xl4bhi@gmail.com>
2021-03-22 01:31:39 +0530

    intial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '780046af292794dd0ddbd2508241dd1e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8bafcc70-bc10-4db3-b5a5-12d73c9a0ef2'
+};
